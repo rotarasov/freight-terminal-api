830ea11482d36be9f366ff741802042f83af9428
rtarasov <portafortuna12@outlook.com>
2021-05-04 01:24:42 +0300

    Minor fixes

--- freight_terminal/settings.py
+++ freight_terminal/settings.py
@@ -67,7 +67,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://127.0.0.1:3000'
 ]
 
 ROOT_URLCONF = 'freight_terminal.urls'
